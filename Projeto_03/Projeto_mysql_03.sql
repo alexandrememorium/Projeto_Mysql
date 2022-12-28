5c6602e778959b84b62af13353bbd3f9aed46f50
alexandrememorium <alexandre.souza@memoriumtech.com>
2022-12-28 15:09:55 -0300

    Atualizando Projeto

---
+++ Projeto_03/Projeto_mysql_03.sql
@@ -0,0 +1,46 @@
+/* 1. Criar um banco de dados para o projeto 2. */
+CREATE DATABASE `proj_php` ;
+
+/* 2. Criar a tabela "cliente" com os seguintes campos: */
+/*     Id_Cliente        */
+/*     NomeCliente       */
+/*     CPF               */
+/*     email             */
+/*     Dt_cadastro       */
+
+CREATE TABLE `proj_php`.`cliente` (
+  `Id_Cliente` INT NOT NULL AUTO_INCREMENT,
+  `NomeCliente` VARCHAR(100) NOT NULL,
+  `CPF` VARCHAR(11) NOT NULL,
+  `email` NCHAR(10) NULL,
+  `Dt_Cadastro` DATE NULL,
+  PRIMARY KEY (`Id`));
+
+/* 3. Criar a tabela "produto" com os seguintes campos: */
+/*     codbarras           */
+/*     NomeProduto        */
+/*     ValorUnitario      */
+/*     Dt_Cadastro         */
+
+CREATE TABLE `proj_pho`.`produto` (
+  `Codbarras` Varchar(20) NOT NULL,
+  `NomeProduto` VARCHAR(100) NULL,
+  `ValorUnitario` Float(30,2) NOT NULL,
+  `Dt_Cadastro` DATE NULL,
+  PRIMARY KEY (`Codbarras`));
+
+/* 4. Criar a tabela "pedido" com os seguintes campos: */
+/*     NumeroPedido      */
+/*     Id_Cliente        */
+/*     CodBarras         */
+/*     Quantidade        */
+/*     DtPedido          */
+
+CREATE TABLE `proj_php`.`pedido` (
+  `NumeroPedido` INT NOT NULL AUTO_INCREMENT,
+  `Id_Cliente` INT NOT NULL,
+  `CodBarras` Varchar(20) NOT NULL,
+  `Quantidader` INT NOT NULL,
+  `Dt_Pedido` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`NumeroPedido`));
+
